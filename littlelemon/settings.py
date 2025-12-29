cc3302575cf75b8bd953d90528d476826e3eed61
Shanthi Priya Ambure <shanthipriya21@example.com>
2025-12-29 21:05:50 +0530

    Change database to SQLite for Render deployment

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'littlelemon',
-        'USER': 'root',
-        'PASSWORD': 'root',  
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
